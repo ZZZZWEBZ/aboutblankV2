c0165c2e3247a2d59f360ad170180d239b33a857
twyutguiwrtguwrytgyrwtgywrtygy <Jacehawkins951@gmail.com>
2025-04-11 20:53:59 -0500

    Create abtblank.js

---
+++ abtblank.js
@@ -0,0 +1,12 @@
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Redirect to about:blank</title>
+            <script>
+            window.location.replace("about:blank");
+            </script>
+        </head>
+        <body>
+            <!-- 0gtnywg3ng4.html -->
+        </body>
+        </html>
